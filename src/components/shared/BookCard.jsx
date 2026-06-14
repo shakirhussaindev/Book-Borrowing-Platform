2d3a6643b0fe17b37ca1eaf9934fa783913cbcce
shakirhussaindev <shakirhusain1414@gmail.com>
2026-06-15 01:09:37 +0600

    added featured books

--- src/components/shared/BookCard.jsx
+++ src/components/shared/BookCard.jsx
@@ -1,31 +1,36 @@
+import { Card } from "@heroui/react";
 import Image from "next/image";
 import Link from "next/link";
 
 const BookCard = ({ book }) => {
-  const { id, title, image_url } = book;
+  const { id, title, image_url, available_quantity } = book;
   return (
-    <div className="card bg-base-100 shadow-lg hover:shadow-2xl transition-all duration-300 border border-base-300">
-      <figure className="h-72 overflow-hidden">
+    <Card className="bg-base-100 shadow-lg hover:shadow-2xl border border-base-300 p-5 hover:border-[#6ad8f1]">
+      <figure className="h-120 w-full">
         <Image
           src={image_url}
           alt={title}
           width={400}
           height={500}
-          className="w-full h-full object-cover"
+          sizes="(max-width: 768px) 100vw, 33vw"
+          className="w-full rounded-md"
         />
       </figure>
 
       {/* Card Body */}
-      <div className="card-body">
-        <h2 className="card-title line-clamp-1">{title}</h2>
+      <div className="">
+        <div className="flex justify-between items-center py-3">
+          <h2 className="text-xl font-medium">{title}</h2>
+          <p className="text-xs text-red-400">{available_quantity} Left</p>
+        </div>
 
-        <div className="card-actions mt-4">
-          <Link href={`/books/${id}`} className="btn btn-primary w-full">
+        <div className="btn bg-[#6ad8f1] w-full text-gray-800">
+          <Link href={`/all-books/${id}`} className="">
             Details
           </Link>
         </div>
       </div>
-    </div>
+    </Card>
   );
 };
 
